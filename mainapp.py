bbc46730ea15238fdbd211a07ef7cee93c042be6
Abhas <2021uee0124@iitjammu.ac.in>
2024-08-15 23:04:28 +0530

    Added some changes

--- mainapp.py
+++ mainapp.py
@@ -1,6 +1,9 @@
 import subprocess
 from sub_main import main
 from selenium import webdriver
+import os
+
+os.makedirs("chrome automation")
 
 cmd = '"chrome.exe" --remote-debugging-port=8989 --user-data-dir="chrome automation"'
 print("Chrome Browser is opened")
