7e6316604c667a7a587fcd6a95a7afe3abb23f5a
Abhas Pastor <108182666+ABHAS2022@users.noreply.github.com>
2024-08-15 23:13:02 +0530

    Added Main File

---
+++ mainapp.py
@@ -0,0 +1,16 @@
+import subprocess
+from sub_main import main
+from selenium import webdriver
+import os
+
+os.makedirs("chrome automation")
+
+cmd = '"chrome.exe" --remote-debugging-port=8989 --user-data-dir="C:\\Users\\ABHAS\\Downloads\\chrome automation"'
+print("Chrome Browser is opened")
+
+# Start the Chrome process in the background
+process = subprocess.Popen(cmd, shell=True)
+
+# Continue with the rest of your code
+main()
+
