d1b13fa054532d1256df5a8d8c473fcc0e8e3816
Abhas <2021uee0124@iitjammu.ac.in>
2024-08-15 22:37:18 +0530

    Adding all the files

---
+++ sub_main.py
@@ -0,0 +1,27 @@
+import os
+import random
+from utils import perform_operation
+
+
+def main():
+    """
+    Main function to execute the script.
+    """
+    print("Starting script...")
+
+    list_of_webaddress_of_chats = [
+        'https://www.pw.live/study/batches/lakshya-jee-2025-095106/batch-overview/mentor-ship/%257B%2522_id%2522%253A%252265d862392b1da90018ad8f3b%2522%252C%2522name%2522%253A%2522Lakshya%2520JEE%25202025%2522%252C%2522conversationId%2522%253A%2522661f6d309d4791f44def1fb8%2522%252C%2522groupId%2522%253A%2522661f6d272a2f23567f2e7da9%2522%257D',
+        'https://www.pw.live/study/batches/lakshya-jee-2025-095106/batch-overview/mentor-ship/%257B%2522_id%2522%253A%252265d862392b1da90018ad8f3b%2522%252C%2522name%2522%253A%2522Lakshya%2520JEE%25202025%2522%252C%2522conversationId%2522%253A%2522661f6d309d4791f44def1fbb%2522%252C%2522groupId%2522%253A%2522661f6d282a2f23567f2e7dfe%2522%257D',
+        'https://www.pw.live/study/batches/lakshya-jee-2025-095106/batch-overview/mentor-ship/%257B%2522_id%2522%253A%252265d862392b1da90018ad8f3b%2522%252C%2522name%2522%253A%2522Lakshya%2520JEE%25202025%2522%252C%2522conversationId%2522%253A%2522661f6d309d4791f44def1fbe%2522%252C%2522groupId%2522%253A%2522661f6d282a2f23567f2e7e53%2522%257D',
+        'https://www.pw.live/study/batches/lakshya-jee-2025-095106/batch-overview/mentor-ship/%257B%2522_id%2522%253A%252265d862392b1da90018ad8f3b%2522%252C%2522name%2522%253A%2522Lakshya%2520JEE%25202025%2522%252C%2522conversationId%2522%253A%2522661f6d319d4791f44def1fc1%2522%252C%2522groupId%2522%253A%2522661f6d282a2f23567f2e7ea6%2522%257D',
+        'https://www.pw.live/study/batches/lakshya-jee-2025-095106/batch-overview/mentor-ship/%257B%2522_id%2522%253A%252265d862392b1da90018ad8f3b%2522%252C%2522name%2522%253A%2522Lakshya%2520JEE%25202025%2522%252C%2522conversationId%2522%253A%2522661f6d319d4791f44def1fc4%2522%252C%2522groupId%2522%253A%2522661f6d282a2f23567f2e7efa%2522%257D'
+    ]
+
+    files_path = 'C:\\Users\\ABHAS\\Downloads\\Maybe Some Idea\\Physics Wallah Automation\\question files'
+    files = [os.path.join(files_path, i) for i in os.listdir(files_path)]
+
+    for url in list_of_webaddress_of_chats:
+        perform_operation(url, random.sample(files,len(files)))
+
+    print("Script finished.")
+
---
+++ utils.py
@@ -0,0 +1,107 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+NUMBER_OF_QUESTIONS = 2
+
+def daily_questions(driver, filenames):
+    """
+    Function to upload files and create polls in the web application.
+
+    Args:
+        driver (webdriver.Chrome): The Selenium WebDriver instance.
+        filenames (list): List of file paths to be uploaded.
+    """
+
+    for filename in filenames[:NUMBER_OF_QUESTIONS]:
+            print(f"Processing file: {filename}")
+
+        # try:
+            # Upload the file
+            print("Waiting for file input element to be visible...")
+            file_input = WebDriverWait(driver, 120).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div/div/div[2]/div[2]/input'))
+            )
+            print("Uploading file...")
+            file_input.send_keys(filename)
+
+            # WebDriverWait(driver,3)
+            # Submit the file
+            time.sleep(4)
+            print("Waiting for submit button to be clickable...")
+            submit_button = WebDriverWait(driver, 120).until(
+                EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[2]/div[2]/div/div/div[2]/div[4]'))
+            )
+            print("Clicking submit button...")
+            submit_button.click()
+            time.sleep(3)
+
+            # Create a poll
+            print("Waiting for create poll button to be clickable...")
+            create_poll_button = WebDriverWait(driver, 120).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div/div/div[1]'))
+            )
+            print("Clicking create poll button...")
+            create_poll_button.click()
+
+            # Fill in poll information
+            print("Filling in poll information...")
+            poll_title = WebDriverWait(driver, 120).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div[1]/div[2]/input'))
+            )
+            poll_title.send_keys("Tick the correct Options")
+
+            options = [
+                [(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/div[2]/input'),"1"],
+                [(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/div[3]/input'), "2"],
+                [(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/div[4]/input'), "3"],
+                [(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/div[5]/input'), "4"]
+            ]
+
+            for xpath, value in options:
+                option = WebDriverWait(driver, 120).until(
+                    EC.presence_of_element_located(xpath)
+                )
+                option.send_keys(value)
+
+            submit_poll = WebDriverWait(driver, 120).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div[4]/button/span/span'))
+            )
+            submit_poll.click()
+            print("Poll created successfully.")
+
+            time.sleep(3)  # Optional, just to ensure any post-submit actions are completed
+
+
+def perform_operation(website, files):
+    """
+    Function to open a website, switch to an iframe, and perform daily tasks.
+
+    Args:
+        website (str): The URL of the website to open.
+        files (list): List of file paths to be uploaded.
+    """
+    print(f"Opening website: {website}")
+
+    opt = Options()
+    opt.add_experimental_option("debuggerAddress", "localhost:8989")
+    driver = webdriver.Chrome(options=opt)
+    driver.get(website)
+
+    print("Waiting for iframe to be available and switching to it...")
+    wait = WebDriverWait(driver, 10)
+    iframe = wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "mentorshipIframe")))
+
+
+    print("Performing daily tasks...")
+    daily_questions(driver, files)
+
+    print("Operation completed.")
+    driver.quit()
